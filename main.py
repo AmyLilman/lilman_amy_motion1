4300f370ebf4b316c12f03b2c20d1ea4b770fe85
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2021-01-27 11:06:40 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 while True:
-    print(input.rotation(Rotation.ROLL))
-    if input.rotation(Rotation.PITCH) > 0:
+    print(input.rotation(Rotation.PITCH))
+    if input.rotation(Rotation.PITCH) > 0 or input.rotation(Rotation.PITCH) < 0:
         light.set_all(light.rgb(0, 0, 255))
     else:
         light.clear()
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 while (true) {
-    console.log(input.rotation(Rotation.Roll))
-    if (input.rotation(Rotation.Pitch) > 0) {
+    console.log(input.rotation(Rotation.Pitch))
+    if (input.rotation(Rotation.Pitch) > 0 || input.rotation(Rotation.Pitch) < 0) {
         light.setAll(light.rgb(0, 0, 255))
     } else {
         light.clear()
