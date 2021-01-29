5130571908cfc586e73b2948e6b95737a3e23cc0
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2021-01-29 16:22:27 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+#while True:
+#    print(input.rotation(Rotation.PITCH))
+#    if input.rotation(Rotation.PITCH) > 0 or input.rotation(Rotation.PITCH) < 0:
+#        light.set_all(light.rgb(0, 0, 255))
+#    else:
+#        light.clear()
+
 while True:
-    print(input.rotation(Rotation.PITCH))
-    if input.rotation(Rotation.PITCH) > 0 or input.rotation(Rotation.PITCH) < 0:
-        light.set_all(light.rgb(0, 0, 255))
+    print(input.acceleration(Dimension.Y))
+    if input.acceleration(Dimension.Y) > 0 or input.acceleration(Dimension.Y) < 0:
+        light.set_all(light.rgb(255, 0, 255))
     else:
         light.clear()
-
--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
+// while True:
+//     print(input.rotation(Rotation.PITCH))
+//     if input.rotation(Rotation.PITCH) > 0 or input.rotation(Rotation.PITCH) < 0:
+//         light.set_all(light.rgb(0, 0, 255))
+//     else:
+//         light.clear()
 while (true) {
-    console.log(input.rotation(Rotation.Pitch))
-    if (input.rotation(Rotation.Pitch) > 0 || input.rotation(Rotation.Pitch) < 0) {
-        light.setAll(light.rgb(0, 0, 255))
+    console.log(input.acceleration(Dimension.Y))
+    if (input.acceleration(Dimension.Y) > 0 || input.acceleration(Dimension.Y) < 0) {
+        light.setAll(light.rgb(255, 0, 255))
     } else {
         light.clear()
     }
