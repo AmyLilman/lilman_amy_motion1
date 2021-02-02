d9d302a3ee1d8281a26b2bd52ca73e0f32c00ad2
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2021-02-02 09:46:34 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@
 
 while True:
     print(input.acceleration(Dimension.X))
-    if input.acceleration(Dimension.X) > 7 or input.acceleration(Dimension.X) < 7:
+    if input.acceleration(Dimension.X) > 0 or input.acceleration(Dimension.X) < 0:
         light.show_animation(light.rainbowAnimation, 500)
         music.play_melody("C C G G A A G F F E E D D C G G F F E E D G G F F E E D C C G G A A G F F E E D D C ", 150)
     else:
