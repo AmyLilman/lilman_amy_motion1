9a3ae178a4d1f50b5fad9d57c072b2e08a2ca3c6
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2021-02-02 09:40:11 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -14,6 +14,7 @@ while (true) {
     console.log(input.acceleration(Dimension.X))
     if (input.acceleration(Dimension.X) > 0 || input.acceleration(Dimension.X) < 0) {
         light.showAnimation(light.rainbowAnimation, 500)
+        music.playMelody("C C G G A A G F F E E D D C G G F F E E D G G F F E E D C C G G A A G F F E E D D C ", 150)
     } else {
         light.clear()
     }
