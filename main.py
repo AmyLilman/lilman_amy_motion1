32cf44e9ecdf63d2c5d65f75ad63fd1cbab0e87c
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2021-02-02 09:52:38 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -14,9 +14,11 @@
 
 while True:
     print(input.acceleration(Dimension.X))
-    if input.acceleration(Dimension.X) > 10 or input.acceleration(Dimension.X) < 10:
+    if input.acceleration(Dimension.X) > 0 or input.acceleration(Dimension.X) < 0:
         light.show_animation(light.rainbowAnimation, 500)
         music.play_melody("C C G G A A G F F E E D D C G G F F E E D G G F F E E D C C G G A A G F F E E D D C ", 150)
+        light.clear()
+        music.stop_all_sounds()
     else:
         light.clear()
         music.stop_all_sounds()
--- main.ts
+++ main.ts
@@ -12,9 +12,11 @@
 //         light.clear()
 while (true) {
     console.log(input.acceleration(Dimension.X))
-    if (input.acceleration(Dimension.X) > 10 || input.acceleration(Dimension.X) < 10) {
+    if (input.acceleration(Dimension.X) > 0 || input.acceleration(Dimension.X) < 0) {
         light.showAnimation(light.rainbowAnimation, 500)
         music.playMelody("C C G G A A G F F E E D D C G G F F E E D G G F F E E D C C G G A A G F F E E D D C ", 150)
+        light.clear()
+        music.stopAllSounds()
     } else {
         light.clear()
         music.stopAllSounds()
