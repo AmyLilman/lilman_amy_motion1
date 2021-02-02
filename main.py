dd1036f30301dd02f2f4a4dbfebc4916ba401b4f
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2021-02-02 15:49:57 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+#while True:
+#    print(input.rotation(Rotation.PITCH))
+#    if input.rotation(Rotation.PITCH) > 0 or input.rotation(Rotation.PITCH) < 0:
+#        light.set_all(light.rgb(0, 0, 255))
+#    else:
+#        light.clear()
 
+#while True:
+#    print(input.acceleration(Dimension.Y))
+#    if input.acceleration(Dimension.Y) > 0 or input.acceleration(Dimension.Y) < 0:
+#        light.set_all(light.rgb(255, 0, 255))
+#    else:
+#        light.clear()
 
 while True:
     print(input.acceleration(Dimension.X))
     if input.acceleration(Dimension.X) > 0 or input.acceleration(Dimension.X) < -20:
         light.show_animation(light.rainbowAnimation, 500)
-        #music.play_melody("C C G G A A G F F E E D D C G G F F E E D G G F F E E D C C G G A A G F F E E D D C ", 150)
+        music.play_melody("C C G G A A G F F E E D D C G G F F E E D G G F F E E D C C G G A A G F F E E D D C ", 150)
     else:
         light.clear()
-        #music.stop_all_sounds()
+        music.stop_all_sounds()
--- main.ts
+++ main.ts
@@ -1,10 +1,23 @@
+// while True:
+//     print(input.rotation(Rotation.PITCH))
+//     if input.rotation(Rotation.PITCH) > 0 or input.rotation(Rotation.PITCH) < 0:
+//         light.set_all(light.rgb(0, 0, 255))
+//     else:
+//         light.clear()
+// while True:
+//     print(input.acceleration(Dimension.Y))
+//     if input.acceleration(Dimension.Y) > 0 or input.acceleration(Dimension.Y) < 0:
+//         light.set_all(light.rgb(255, 0, 255))
+//     else:
+//         light.clear()
 while (true) {
     console.log(input.acceleration(Dimension.X))
     if (input.acceleration(Dimension.X) > 0 || input.acceleration(Dimension.X) < -20) {
         light.showAnimation(light.rainbowAnimation, 500)
+        music.playMelody("C C G G A A G F F E E D D C G G F F E E D G G F F E E D C C G G A A G F F E E D D C ", 150)
     } else {
-        // music.play_melody("C C G G A A G F F E E D D C G G F F E E D G G F F E E D C C G G A A G F F E E D D C ", 150)
         light.clear()
+        music.stopAllSounds()
     }
     
 }
