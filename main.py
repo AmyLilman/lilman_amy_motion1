b12323d13ce578f7a778b7a4edf1d93d039a8c0a
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2021-02-02 10:04:00 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ while True:
     print(input.acceleration(Dimension.X))
     if input.acceleration(Dimension.X) > 0 or input.acceleration(Dimension.X) < 0:
         light.show_animation(light.rainbowAnimation, 500)
-        #music.play_melody("C C G G A A G F F E E D D C G G F F E E D G G F F E E D C C G G A A G F F E E D D C ", 150)
+        music.play_melody("C C G G A A G F F E E D D C G G F F E E D G G F F E E D C C G G A A G F F E E D D C ", 150)
         light.clear()
-        #music.stop_all_sounds()
+        music.stop_all_sounds()
     else:
         light.clear()
-        #music.stop_all_sounds()
+        music.stop_all_sounds()
